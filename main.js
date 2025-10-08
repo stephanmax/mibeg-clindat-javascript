817e999e7cac08b636ec561f7adcad72fb5ebe9d
Stephan Max <stephanmax@users.noreply.github.com>
2025-10-08 07:13:00 +0000

    New web applications

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello, World!");
+// Funktionsausdruck (expression)
+const add = (a, b) => {
+    return a + b;
+};
+
+// Funktionsdeklaration (Anweisung, statement)
+// function add(a, b) {
+//     return a+b;
+// }
+
+console.log(add(5, 10));
+
+// Ausdrücke, z.B. 5, "Hallo", 5 < 10, 3 + 7, Funktionen
+// Anweisungen, z.B. console.log("")
+
+// Schreiben Sie eine Funktion `celsiusToFahrenheit()`, die Grad Celsius in Grad Fahrenheit umwandelt.
+
+const celsiusToFahrenheit = (celsius) => {
+  const fahrenheit = (celsius * (9/5)) + 32;
+  return fahrenheit;
+};
